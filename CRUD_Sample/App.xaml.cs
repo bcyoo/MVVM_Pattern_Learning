db133b82d37d5449d28c4038be9763d4c86bf290
bcyoo <89197951+bcyoo@users.noreply.github.com>
2023-07-26 14:13:56 +0900

    MVVM_test_01

---
+++ CRUD_Sample/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CRUD_Sample
+{
+    /// <summary>
+    /// App.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            Services = ConfigureServices();
+            this.InitializeComponent();
+        }
+
+        public new static App Current => (App)Application.Current;
+
+        public IServiceProvider Services { get; }
+
+        private static IServiceProvider ConfigureServices()
+        {
+            var services = new ServiceCollection();
+
+            services.AddTransient(typeof(MainViewModel));
+            return services.BuildServiceProvider();
+        }
+
+
+    }
+}
